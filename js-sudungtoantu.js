e36e9945f916765c90d32425b3059af6378c5748
Nguyễn Huỳnh Đức <nguyenhuynhduc96vp@gmail.com>
2021-07-08 16:46:11 +0700

    them bai th toan tu

---
+++ js-sudungtoantu.js
@@ -0,0 +1,8 @@
+    let inputWidth;
+    let inputHeight;
+    inputWidth = prompt("Nhập vào chiều rộng");
+    inputHeight = prompt(" Nhập vào chiều cao")
+    let width = parseInt(inputWidth);
+    let hight = parseInt(inputHeight);
+    let area = width * hight;
+    document.write("Diện tích =" + area)
